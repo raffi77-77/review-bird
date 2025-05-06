259e356e62f35b5956aa7a10a2d79099b09d6f94
Stepan <stepanbadalyan8@gmail.com>
2025-05-06 11:54:45 +0400

    Added: link to the flow

--- blocks/flow/src/components/flow.js
+++ blocks/flow/src/components/flow.js
@@ -9,47 +9,51 @@ export default function Flow({id, data}) {
         <Utilities/>
         {step === 1 && <div className='rw-flow-container'>
 
-            <div className='rw-flow-feedback-row'>
-                <div className="rw-flow-feedback-header">
-                    <div className="rw-flow-logo">
-                        <img src={logo}/>
+            <div className="rw-flow-feedback-cont">
+                <div className='rw-flow-feedback-row'>
+                    <div className="rw-flow-feedback-header">
+                        <div className="rw-flow-logo">
+                            <img src={logo}/>
+                        </div>
+                        <button className="rw-flow-button-close rw-flow-button-30">
+                            <svg className='rw-flow-button-close-i' xmlns='http://www.w3.org/2000/svg'
+                                 fill='none' viewBox='0 0 24 24'>
+                                <use href='#rw-flow-close'/>
+                            </svg>
+                        </button>
                     </div>
-                    <button className="rw-flow-button-close rw-flow-button-30">
-                        <svg className='rw-flow-button-close-i' xmlns='http://www.w3.org/2000/svg'
-                             fill='none' viewBox='0 0 24 24'>
-                            <use href='#rw-flow-close'/>
-                        </svg>
-                    </button>
-                </div>
-                <div className="rw-flow-feedback-body">
-                    <div className="rw-flow-title">
-                        <p className="rw-flow-title-in"> Würden Sie {`{site-name}`} <br/> weiterempfehlen? </p>
-                    </div>
+                    <div className="rw-flow-feedback-body">
+                        <div className="rw-flow-title">
+                            <p className="rw-flow-title-in"> Würden Sie {`{site-name}`} <br/> weiterempfehlen? </p>
+                        </div>
+                        <div className="rw-flow-feedback-actions">
+                            <div className="rw-flow-feedback-actions-in">
+                                <button
+                                    className='rw-flow-button rw-flow-button-feedback rw-flow-button-feedback-good'>
+                                    <div className='rw-flow-button-feedback-in'>
+                                        <svg className='rw-flow-button-feedback-i' xmlns='http://www.w3.org/2000/svg'
+                                             fill='none' viewBox='0 0 24 24'>
+                                            <use href='#rw-flow-thumb-up'/>
+                                        </svg>
+                                    </div>
+                                </button>
 
-                    <div className="rw-flow-feedback-actions">
-                        <div className="rw-flow-feedback-actions-in">
-                            <button
-                                className='rw-flow-button rw-flow-button-feedback rw-flow-button-feedback-good'>
-                                <div className='rw-flow-button-feedback-in'>
-                                    <svg className='rw-flow-button-feedback-i' xmlns='http://www.w3.org/2000/svg'
-                                         fill='none' viewBox='0 0 24 24'>
-                                        <use href='#rw-flow-thumb-up'/>
-                                    </svg>
-                                </div>
-                            </button>
-
-                            <button
-                                className='rw-flow-button rw-flow-button-feedback rw-flow-button-feedback-bad'>
-                                <div className='rw-flow-button-feedback-in'>
-                                    <svg className='rw-flow-button-feedback-i' xmlns='http://www.w3.org/2000/svg'
-                                         fill='none' viewBox='0 0 24 24'>
-                                        <use href='#rw-flow-thumb-down'/>
-                                    </svg>
-                                </div>
-                            </button>
+                                <button
+                                    className='rw-flow-button rw-flow-button-feedback rw-flow-button-feedback-bad'>
+                                    <div className='rw-flow-button-feedback-in'>
+                                        <svg className='rw-flow-button-feedback-i' xmlns='http://www.w3.org/2000/svg'
+                                             fill='none' viewBox='0 0 24 24'>
+                                            <use href='#rw-flow-thumb-down'/>
+                                        </svg>
+                                    </div>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <a href="#" className="rw-flow-feedback-link">
+                    <span>Uberspringen und offentlich posten</span>
+                </a>
             </div>
 
             <div className='rw-flow-row rw-flow-feedback-row none'>
