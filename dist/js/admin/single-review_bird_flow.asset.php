2afdf4226fd5d008f52c118193ca6b62326a3ab0
vahan889 <vahangrigoryan889@gmail.com>
2025-05-22 15:09:58 +0400

    Merged from master.

--- dist/js/admin/single-review_bird_flow.asset.php
+++ dist/js/admin/single-review_bird_flow.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '013ba42b045a00a83a68');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '14b884fc9e6f4eb0e591');
