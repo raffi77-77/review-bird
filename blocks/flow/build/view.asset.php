ca818c39d0ef64a1e86a0fb432a9bfd07c059389
vahan889 <vahangrigoryan889@gmail.com>
2025-05-23 21:22:23 +0400

    Merged with UI.

--- blocks/flow/build/view.asset.php
+++ blocks/flow/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'cbc5fa91befc0318b43e');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'adf1f18e431030ca3f4f');
