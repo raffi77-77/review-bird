e2be4c80f949926d7b6b24b9a366ad524664fbe7
raffi77_77 <raffi.yeghiazaryan@gmail.com>
2025-05-23 19:33:01 +0400

    Added: freemius icon setting

--- includes/services/class-rb-freemius.php
+++ includes/services/class-rb-freemius.php
@@ -3,20 +3,21 @@
 namespace Review_Bird\Includes\Services;
 
 use Exception;
+use Review_Bird\Includes\Review_Bird;
 use Review_Bird\Includes\Traits\SingletonTrait;
 
 class Freemius {
-	
+
 	use SingletonTrait;
 
 	const PREMIUM_OPTION_KEY = 'review_bird_premium';
-	
+
 	public ?\Freemius $fs;
 
 	public string $plugin_path;
 
 	public function __construct() {
-		$this->fs             = $this->fs_init();
+		$this->fs = $this->fs_init();
 		if ( ! empty( $fs ) ) {
 			$this->fs->add_filter( 'plugin_icon', [ $this, 'plugin_icon' ] );
 		}
@@ -25,17 +26,18 @@ class Freemius {
 	private function fs_init(): ?\Freemius {
 		try {
 			return fs_dynamic_init( array(
-				'id'                  => '19172',
-				'slug'                => 'review-bird',
-				'premium_slug'        => 'review-bird-pro',
-				'type'                => 'plugin',
-				'public_key'          => 'pk_7c426b1d7d57bb7e3852aa1d431eb',
-				'is_premium'          => true,
-				'premium_suffix'      => '(PRO)',
-				'has_addons'          => false,
-				'has_paid_plans'      => true,
-				'menu'                => array(
-					'slug'           => 'review-bird',
+				'id'             => '19172',
+				'slug'           => 'review-bird',
+				'premium_slug'   => 'review-bird-pro',
+				'type'           => 'plugin',
+				'public_key'     => 'pk_7c426b1d7d57bb7e3852aa1d431eb',
+				'is_premium'     => true,
+				'premium_suffix' => '(PRO)',
+				'has_addons'     => false,
+				'has_paid_plans' => true,
+				'menu'           => array(
+					'slug' => 'review-bird',
+					'icon' => Review_Bird::get_instance()->get_plugin_dir_url() . 'resources/admin/icon.png',
 				),
 			) );
 		} catch ( Exception $e ) {
