70852e2836abc456b163f4541e465156dc44c9ca
vahan889 <vahangrigoryan889@gmail.com>
2025-05-23 21:51:05 +0400

    Fixed: settings fetch query data.

--- assets/js/admin/posts/data.js
+++ assets/js/admin/posts/data.js
@@ -1,1 +1,15 @@
-export const SETTINGS_KEYS_PREFIX = 'review_bird_utility_flow_';
+export const SETTINGS_KEYS_PREFIX = 'review_bird_utilities_flow_';
+
+export const SETTINGS_KEYS = [
+    'question',
+    // 'targets',
+    // 'target_distribution',
+    // 'multiple_targets',
+    'review_box_text',
+    'username_placeholder',
+    'review_placeholder',
+    'success_message',
+    // 'gating',
+    // 'email_notify_on_negative_review',
+    'emails_on_negative_review',
+];
--- dist/js/admin/single-review_bird_flow.asset.php
+++ dist/js/admin/single-review_bird_flow.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '978eff0a071eceaf710a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '497e8ef01a3f51428539');
