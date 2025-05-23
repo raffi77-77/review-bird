23dc8d7f5f8eab6d611d602c8e6651354986856d
vahan889 <vahangrigoryan889@gmail.com>
2025-05-23 20:00:49 +0400

    Fixed: get back the tooltip.

--- dist/js/admin/single-review_bird_flow.asset.php
+++ dist/js/admin/single-review_bird_flow.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '909153b8d23e4654c08b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '80a68a9d81597a0e6ba7');
